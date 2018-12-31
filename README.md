df744b1c56487e441849c3027f9f312eb31d9a97
Tony Yang <tyang27@jhu.edu>
2018-12-31 01:18:21 -0500

    My god, css is so nice now?

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Academic Website for Tony Y
+
+```
+source bin/activate.sh
+pip install -r requirements.txt
+```
+If you get this error:
+```
+error: could not create '/Library/Python/2.7/site-packages/blinker': Permission denied
+```
+Resolve by running code [ref](https://github.com/sindresorhus/weechat-notification-center/issues/1):
+```
+sudo chown -R $USER /Library/Python/2.7
+```
---
+++ app1/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+application = Flask(__name__, static_url_path='/static')
+
+templateLayout = 'layout.html'
+templateIndex = 'index.html'
+templateTech = 'tech.html'
+
+#=============================================================================#
+# Handles Route /
+@application.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template(templateIndex, layoutfp=templateLayout)
+
+@application.route('/tech', methods=['GET', 'POST'])
+def tech():
+    return render_template(templateTech, layoutfp=templateLayout)
+
+#=============================================================================#
+# https://stackoverflow.com/questions/21714653/flask-css-not-updating
+@application.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(application.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+#=============================================================================#
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
